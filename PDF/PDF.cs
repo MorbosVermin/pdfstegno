a6396dbd4af32f8c2b2d9fab3cf929edf526f835
Mike Duncan <morbos.vermin@gmail.com>
2016-08-09 00:16:32 -0400

    Initial commit.

---
+++ PDF/PDF.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PDF
+{
+
+    public sealed class PDFHeader
+    {
+
+        public string Version
+        {
+            get;
+            set;
+        }
+
+        public bool isBinary
+        {
+            get;
+            set;
+        }
+
+        public PDFHeader(string version, bool isBinary)
+        {
+            this.Version = version;
+            this.isBinary = isBinary;
+        }
+
+        public PDFHeader(string version) : this(version, false) {}
+
+        public PDFHeader()  : this("1.1", false) {}
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append(String.Format("%PDF-{0}", Version) + PDFDoc.CRLF);
+            if(isBinary)
+                str.Append("%\xD0\xD0\xD0\xD0" + PDFDoc.CRLF);
+
+            return str.ToString();
+        }
+
+    }
+
+    public sealed class PDFXRef
+    {
+
+        public int Offset
+        {
+            get;
+            set;
+        }
+
+        public int GenerationNumber
+        {
+            get;
+            set;
+        }
+
+        public string Flags
+        {
+            get;
+            set;
+        }
+
+        public PDFXRef(int offset, int genNumber, string flags)
+        {
+            Offset = offset;
+            GenerationNumber = genNumber;
+            Flags = flags;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2}", Offset.ToString("D10"), GenerationNumber.ToString("D5"), Flags);
+        }
+
+    }
+
+    public sealed class PDFXRefs : CollectionBase
+    {
+
+        public PDFXRefs()
+            : base()
+        {
+            this.Add(new PDFXRef(0, 65535, "f"));
+        }
+
+        public int Add(PDFXRef xref)
+        {
+            return List.Add(xref);
+        }
+
+        public bool Contains(PDFXRef xref)
+        {
+            foreach(PDFXRef xr in this)  
+            {
+                if (xref.Offset == xr.Offset)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public PDFXRef this[int index]
+        {
+            get { return (PDFXRef)List[index]; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append("xref" + PDFDoc.CRLF);
+            str.Append("0 " + (Count - 1) + PDFDoc.CRLF);
+            foreach (PDFXRef xref in this)
+            {
+                str.Append(xref.ToString() + PDFDoc.CRLF);
+            }
+
+            return str.ToString();
+        }
+
+    }
+
+    public sealed class PDFTrailor
+    {
+        public int Start
+        {
+            get;
+            set;
+        }
+
+        public int Size
+        {
+            get;
+            set;
+        }
+
+        public string Root
+        {
+            get;
+            set;
+        }
+
+        public string Info
+        {
+            get;
+            set;
+        }
+
+        public string Previous
+        {
+            get;
+            set;
+        }
+
+        public PDFTrailor(int start, int size, string root, string info)
+        {
+            Start = start;
+            Size = size;
+            Root = root;
+            Info = info;
+            Previous = null;
+        }
+
+        public PDFTrailor(int start, int size, string root) : this(start, size, root, null) { }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append("trailor" + PDFDoc.CRLF);
+            str.Append(String.Format("<<" + PDFDoc.CRLF + " /Size {0}" + PDFDoc.CRLF, Size));
+            str.Append(String.Format(" /Root {0}" + PDFDoc.CRLF, Root));
+            
+            if (Previous != null)
+                str.Append(String.Format(" /Previous {0}"+ PDFDoc.CRLF, Previous));
+
+            if (Info != null)
+                str.Append(String.Format(" /Info {0}" + PDFDoc.CRLF, Info));
+            
+            str.Append(String.Format(">>" + PDFDoc.CRLF + "startxref" + PDFDoc.CRLF + "{0}" + PDFDoc.CRLF + "%%EOF", Start));
+            return str.ToString();
+        }
+
+    }
+
+    public enum PDFObjectTypes
+    {
+        Catalog,
+        Outlines,
+        Pages,
+        Page
+    }
+
+    public sealed class PDFObject
+    {
+
+        public int Index
+        {
+            get;
+            set;
+        }
+
+        public int Version
+        {
+            get;
+            set;
+        }
+
+        public PDFObjectTypes Type
+        {
+            get;
+            set;
+        }
+
+        public string Data
+        {
+            get;
+            set;
+        }
+        public PDFObject(int index, int version, PDFObjectTypes type, string data)
+        {
+            Index = index;
+            Version = version;
+            Type = type;
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append(String.Format("%d %d obj", Index, Version) + PDFDoc.CRLF);
+            str.Append(String.Format("<<" + PDFDoc.CRLF +" /Type /%s", Type) + PDFDoc.CRLF);
+            str.Append(Data + PDFDoc.CRLF);
+            str.Append(">>" + PDFDoc.CRLF +"endobj");
+
+            return str.ToString();
+        }
+
+    }
+
+    public sealed class PDFDoc
+    {
+        public static string CRLF { get { return "\n"; } }
+
+        public string Path
+        {
+            get;
+            set;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return File.ReadAllBytes(Path).Length;
+            }
+        }
+
+        public PDFHeader Header
+        {
+            get;
+            set;
+        }
+
+        private PDFXRefs References;
+
+        public PDFDoc(string path, bool binary)
+        {
+            References = new PDFXRefs();
+            Path = path;
+            if (!File.Exists(Path))
+            {
+                Header = new PDFHeader();
+                Header.isBinary = binary;
+            }
+
+        }
+
+        public PDFDoc(string path) : this(path, false) { }
+
+        public void StartNew()
+        {
+            using (StreamWriter writer = new StreamWriter(File.Open(Path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(Header.ToString());
+                writer.Flush();
+            }
+        }
+
+        public void Append(string line)
+        {
+            if (!line.EndsWith(CRLF))
+                line += CRLF;
+
+            using (StreamWriter writer = new StreamWriter(File.Open(Path, FileMode.Append)))
+            {
+                writer.Write(line);
+                writer.Flush();
+            }
+        }
+
+        public void AppendNewLine()
+        {
+            Append("");
+        }
+
+        public void Append(byte[] data)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(Path, FileMode.Append), Encoding.ASCII))
+            {
+                writer.Write(data, 0, data.Length);
+                writer.Flush();
+            }
+        }
+
+        public void AppendComment(string comment)
+        {
+            Append(String.Format("%% %s", comment));
+        }
+
+        public static string ToHex(byte[] data)
+        {
+            StringBuilder b = new StringBuilder(data.Length * 2);
+            foreach(byte d in data)  
+            {
+                b.AppendFormat("{0:x2}", d);
+            }
+
+            return b.ToString();
+        }
+
+        public void AppendHexEncodedStream(int index, int version, byte[] data)
+        {
+            string eData = ToHex(data);
+            AppendNewLine();
+            References.Add(new PDFXRef(index, Size, "n"));
+            Append(String.Format("{0} {1} obj" + CRLF + "<<"+ CRLF +" /Length {2}"+ CRLF +" /Filter ASCIIHexDecode"+ CRLF +">>", index, version, eData.Length));
+            Append(Encoding.ASCII.GetBytes(eData));
+            Append(CRLF + "endstream" + CRLF + "endobj");
+        }
+
+        public void AppendXRef()
+        {
+            AppendNewLine();
+            Append(References.ToString());
+        }
+
+        public void AppendTrailor(string root)
+        {
+            PDFTrailor trailer = new PDFTrailor(Size, References.Count - 1, root);
+            AppendNewLine();
+            Append(trailer.ToString());
+        }
+
+        public void IndirectObject(int index, int version, string io)
+        {
+            AppendNewLine();
+            References.Add(new PDFXRef(Size, version, "n"));
+            Append(String.Format("{0} {1} obj"+ CRLF +"{2}"+ CRLF +"endobj", index, version, io));
+        }
+
+    }
+
+}
---
+++ pdfstegno/Program.cs
@@ -0,0 +1,300 @@
+﻿using PDF;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace pdfstegno
+{
+    public class Program
+    {
+
+        /// <summary>
+        /// TODO - No worky! :(
+        /// 
+        /// I initially used code from Dieder Stevens to create a PDF file, however this does not
+        /// seem to be working now with modern PDF readers. 
+        /// </summary>
+        /// <param name="path"></param>
+        static void CreatePdf(string path)
+        {
+            PDFDoc doc = new PDFDoc(path);
+            doc.StartNew();
+            doc.IndirectObject(1, 0, "<<" + PDFDoc.CRLF + " /Type /Catalog" + PDFDoc.CRLF + " /Outlines 2 0 R" + PDFDoc.CRLF + " /Pages 3 0 R " + PDFDoc.CRLF + ">>");
+            doc.IndirectObject(2, 0, "<<" + PDFDoc.CRLF + " /Type /Outlines" + PDFDoc.CRLF + " /Count 0" + PDFDoc.CRLF + ">>");
+            doc.IndirectObject(3, 0, "<<" + PDFDoc.CRLF + " /Type /Pages" + PDFDoc.CRLF + " /Kids [4 0 R]" + PDFDoc.CRLF + ">>");
+            doc.IndirectObject(4, 0, "<<" + PDFDoc.CRLF + " /Type /Page" + PDFDoc.CRLF + " /Parent 3 0 R" + PDFDoc.CRLF + ">>");
+
+            doc.AppendXRef();
+            doc.AppendTrailor("1 0 R");
+        }
+
+        /// <summary>
+        /// Given a source (PDF) file, a destination, a name for referencing the payload within the PDF, and
+        /// finally a executable path (payload), we open the source PDF, recalucate the xref with the 
+        /// payload and inject the payload into the PDF which is written to destionation. 
+        /// </summary>
+        /// <param name="source">Source PDF file.</param>
+        /// <param name="destination">Destination PDF file.</param>
+        /// <param name="name">Reference Name</param>
+        /// <param name="execPath">Payload</param>
+        static void Stegno(string source, string destination, string name, string execPath)
+        {
+            byte[] buffer = File.ReadAllBytes(execPath); //TODO may not work on large files.
+            string payload = System.Convert.ToBase64String(buffer, 0, buffer.Length, Base64FormattingOptions.InsertLineBreaks);
+            string[] base64 = payload.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            Console.WriteLine();
+            Console.WriteLine("Source: " + source);
+            Console.WriteLine("Payload: " + execPath + " (" + payload.Length + "b; " + base64.Length + " loc)");
+            Console.WriteLine("------------------------------------------------");
+
+            /*
+             * Find xref
+             * Inject content
+             * Update startxref to include payload (new xref position).
+             * Profit?
+             */
+            int xref = 0;
+            int total = 0;
+            Console.Write("Finding current xref location; please wait...");
+            using (Stream s = File.Open(source, FileMode.Open))
+            {
+                while (xref == 0)
+                {
+                    byte[] b2 = new byte[1024];
+                    int i = s.Read(b2, 0, b2.Length);
+                    if (i == -1)
+                        break;
+
+                    total += i;
+                    string content = Encoding.ASCII.GetString(b2, 0, b2.Length);
+                    if (content.IndexOf("xref") >= 0)
+                    {
+                        xref = total - (i - content.IndexOf("xref"));
+                    }
+                }
+            }
+            Console.WriteLine("done: " + xref);
+
+            if (File.Exists(destination))
+                File.Delete(destination);
+
+            int payloadTotal = 0;
+            total = File.ReadAllBytes(source).Length;
+            byte[] b = new byte[xref];
+            using (Stream s = File.Open(source, FileMode.Open))
+            {
+                using (Stream d = File.Open(destination, FileMode.OpenOrCreate))
+                {
+                    int i = -1;
+                    int t = 0;
+                    while (true)
+                    {
+                        i = s.Read(b, 0, b.Length);
+                        t += i;
+                        d.Write(b, 0, i);
+                        d.Flush();
+
+                        if (t == xref)
+                        {
+                            Console.Write("Injecting payload into " + destination + " by name '" + name + "'...");
+                            byte[] l = Encoding.ASCII.GetBytes("% " + name + PDFDoc.CRLF);
+                            payloadTotal = l.Length;
+                            d.Write(l, 0, l.Length);
+                            d.Flush();
+
+                            foreach (string p in base64)
+                            {
+                                if (p.Length == 0)
+                                    continue;
+
+                                byte[] l2 = Encoding.ASCII.GetBytes("% " + p + PDFDoc.CRLF);
+                                payloadTotal += l2.Length;
+                                d.Write(l2, 0, l2.Length);
+                                d.Flush();
+                            }
+                            Console.WriteLine("done: " + payloadTotal + "bytes.");
+                        }
+
+
+                        if (t == total)
+                            break;
+
+                    }
+                }
+            }
+
+            buffer = new byte[128];
+            total = File.ReadAllBytes(destination).Length;
+            using (Stream stream = File.Open(destination, FileMode.Open))
+            {
+                stream.Position = total - buffer.Length;
+                int i = stream.Read(buffer, 0, buffer.Length);
+                string l = Encoding.ASCII.GetString(buffer, 0, i);
+                if (l.LastIndexOf("startxref") > 0)
+                {
+                    stream.Position = (total - (buffer.Length - l.LastIndexOf("startxref"))) + 10;
+                    Console.Write("Updating startxref from " + xref + " to " + (xref + payloadTotal) + " at " + stream.Position + "; please wait...");
+                    byte[] d = Encoding.ASCII.GetBytes(PDFDoc.CRLF + (xref + payloadTotal) + PDFDoc.CRLF + "%%EOF" + PDFDoc.CRLF);
+                    stream.Write(d, 0, d.Length);
+                    stream.Flush();
+                    Console.WriteLine("done.");
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Look within the given PDF file (path) for the given reference name and use the xref to extract 
+        /// the payload. The payload is returned as a byte[]. 
+        /// </summary>
+        /// <param name="path">Path to the PDF file to extract the payload from.</param>
+        /// <param name="name">Reference name of the payload within the PDF file.</param>
+        /// <returns>byte[] the payload</returns>
+        static byte[] GetPayload(string path, string name)
+        {
+            StringBuilder base64 = new StringBuilder();
+            using (StreamReader reader = File.OpenText(path))
+            {
+                string line = "";
+                bool start = false;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Trim().Equals("% " + name))
+                    {
+                        start = true;
+                        continue;
+                    }
+                    else if (line.Trim().Equals("xref"))
+                        break;
+
+                    if (start)
+                    {
+                        base64.AppendLine(line.Substring(2));
+                    }
+
+
+                }
+
+                if (!start)
+                {
+                    Console.WriteLine("Error: No content found by marker/name '" + name + "'");
+                }
+                else
+                {
+                    Console.WriteLine("Payload '" + name + "' retrieved: " + base64.Length + "bytes (encoded).");
+                }
+            }
+
+            return (base64.Length > 0) ? System.Convert.FromBase64String(base64.ToString()) : new byte[0];
+        }
+
+        /// <summary>
+        /// Thank you mister helper, helping-ton!
+        /// </summary>
+        static void Help()
+        {
+            Console.WriteLine("PDF Stegnography Tool");
+            Console.WriteLine();
+            Console.WriteLine("Syntax: {0} <path to .pdf file> [-o <destination>] [-x <payload>] [-n <ref name>] [-e]",
+                Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName));
+            Console.WriteLine("/o   The destination file to write results too.");
+            Console.WriteLine("/n   Use this name for the marker of the content within the PDF.");
+            Console.WriteLine("/x   Inject the content of this .exe into the PDF.");
+            //Console.WriteLine("/N   Create a new, blank PDF file to work with rather than supply one.");
+            Console.WriteLine("/e   Extract the .exe from the PDF and save to the file destination given (-o). Use -n to change name/marker to search for.");
+            Console.WriteLine();
+            Console.WriteLine("Example Usage: ");
+            Console.WriteLine("   pdfstegno.exe /x payload.exe /o phish_hook.pdf MyInitial.pdf");
+            Console.WriteLine("   pdfstegno.exe /e /o payload2.exe phish_hook.pdf");
+            Environment.Exit(1);
+        }
+
+        /// <summary>
+        /// Application Entry Point
+        /// </summary>
+        /// <param name="args">Arguments to the application.</param>
+        /// <see cref="Help"/>
+        static void Main(string[] args)
+        {
+            string name = "0x00000003";
+            string source = "";
+            string destination = "";
+            string execPath = "";
+            bool extract = false;
+            bool newPdf = false;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].Equals("/h") || args[i].Contains("-help"))
+                    Help();
+
+                if (args[i].Equals("/n"))
+                {
+                    name = args[(i + 1)];
+                    i++;
+                }
+                else if (args[i].Equals("/o"))
+                {
+                    destination = args[(i + 1)];
+                    i++;
+                }
+                else if (args[i].Equals("/x"))
+                {
+                    execPath = args[(i + 1)];
+                    i++;
+                }
+                else if(args[i].Equals("/N"))
+                {
+                    source = Path.GetRandomFileName();
+                    source = String.Format("{0}.pdf", source.Substring(0, source.LastIndexOf('.')));
+                    newPdf = true;
+                }
+                else if (args[i].Equals("/e"))
+                    extract = true;
+
+                else
+                    source = args[i];
+
+            }
+
+            if (source.Length == 0)
+                Help();
+
+            else if(!File.Exists(source) && newPdf)
+                CreatePdf(source);
+
+            else if(!File.Exists(source))
+            {
+                Console.Error.WriteLine("Error: File not found: {0}", source);
+                Environment.Exit(1);
+            }
+
+            if (extract)
+            {
+                byte[] payload = GetPayload(source, name);
+                if (payload.Length > 0)
+                {
+                    using (Stream s = File.Open(destination, FileMode.OpenOrCreate))
+                    {
+                        s.Write(payload, 0, payload.Length);
+                        s.Flush();
+                    }
+                    Console.WriteLine("Wrote " + destination + " (" + payload.Length + "bytes)");
+                }
+
+            }
+            else if (execPath.Length > 0)
+            {
+                Stegno(source, destination, name, execPath);
+            }
+            else if (destination.Length == 0)
+            {
+                GetPayload(source, name);
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
